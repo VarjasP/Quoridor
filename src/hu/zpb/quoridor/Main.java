ecd0b92649800d215ea620f809009ca2b2f11aae
Varjas István Péter <istvan.varjas@generalmechatronics.com>
2021-05-09 10:19:25 +0200

    GameTRX playerJoined

--- src/hu/zpb/quoridor/Main.java
+++ src/hu/zpb/quoridor/Main.java
@@ -10,9 +10,11 @@ public class Main {
 
     public static void main(String[] args) {
         GUI gameGUI = new GUI();
+        GameModel gm = new GameModel();
+
         gameGUI.drawMenu();
 
-        GameModel gm = new GameModel();
+
 
         GameTRX g = GameTRX.getInstance();
         g.createServer();
@@ -21,6 +23,11 @@ public class Main {
             public void networkEventCallback(GameModel data) {
                 g.sendGameEvent(data);
             }
+
+            @Override
+            public void playerJoined(Player player) {
+
+            }
         });
 
         g.setNetworkEvent(new GameTRX.NetworkEvent() {
@@ -28,12 +35,21 @@ public class Main {
             public void networkEventCallback(GameModel data) {
                 System.out.println("GameModel érkezett");
             }
+
+            @Override
+            public void playerJoined(Player player) {
+                System.out.println("Player érkezett");
+            }
         });
+
         g.createClient();
+        g.joinPlayer(new Player(new Point(4,0), Color.BLACK, 1, "én", 10));
         g.sendGameEvent(gm);
 
         g.createClient();
+        g.joinPlayer(new Player(new Point(4,0), Color.BLUE, 1, "rattyer", 10));
         g.createClient();
+        g.joinPlayer(new Player(new Point(4,0), Color.RED, 1, "géza", 10));
         g.sendGameEvent(gm);
     }
 }
